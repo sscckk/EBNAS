d0ee14a3249a7d2b889c0d8192cb98d54e256268
sscckk <46714152+sscckk@users.noreply.github.com>
2022-12-12 16:02:40 +0800

    Update search.sh

--- search.sh
+++ search.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-method="imagenet"
-gpu="5,6,7"
+method="EBNAS"
+gpu="5"
 
-# nohup python -W ignore train_search.py --save ${method} --gpu ${gpu} &
+nohup python -W ignore train_search.py --save ${method} --gpu ${gpu} &
 
-nohup python -W ignore train_search_imagenet.py --save ${method} --gpu ${gpu} &
+

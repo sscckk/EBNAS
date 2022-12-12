9240bd4584643461ab11227a21f68c77f8f33f81
sscckk <46714152+sscckk@users.noreply.github.com>
2022-12-12 16:05:24 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -3,8 +3,8 @@
 dataset="imagenet"
 layers="14"
 init_channels="96"
-arch="s91_16b2"
-suf="s91_16b2-14-96"
+arch="EBNAS"
+suf="EBNAS-14-96"
 gpu="0,1"
 
 # nohup python -W ignore train_cifar10.py --layers ${layers} --init_channels ${init_channels} --arch ${arch} --save cifar10-${arch}-${layers}-${init_channels} --cutout --auxiliary --gpu ${gpu} --binary &

6439bfde9d67f13a94d66cfb66a84ebdc5c98dd8
sscckk <46714152+sscckk@users.noreply.github.com>
2022-12-12 15:58:42 +0800

    Update genotypes.py

--- genotypes.py
+++ genotypes.py
@@ -13,3 +13,5 @@ PRIMITIVES = [
 ]
 
 EBNAS = Genotype(normal=[('bin_conv_3x3', 0), ('max_pool_3x3', 1), ('max_pool_3x3', 0), ('max_pool_3x3', 1), ('bin_conv_3x3', 1), ('bin_conv_5x5', 0), ('max_pool_3x3', 0), ('bin_dil_conv_3x3', 1)], normal_concat=range(2, 6), reduce=[('bin_conv_5x5', 0), ('bin_conv_5x5', 1), ('bin_conv_3x3', 1), ('bin_dil_conv_5x5', 2), ('bin_dil_conv_5x5', 2), ('bin_conv_3x3', 1), ('max_pool_3x3', 0), ('bin_conv_3x3', 1)], reduce_concat=range(2, 6))
+
+
